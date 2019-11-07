8bbec8f27622eb597acbc0a6d8412d26854ef760
solomonsunday <schinoyerem007@gmail.com>
2019-11-07 21:04:25 +0100

    Working on checkout page

--- app/views/shoping-cart/controllers/create-shopping-cart.js
+++ app/views/shoping-cart/controllers/create-shopping-cart.js
@@ -11,6 +11,30 @@
             vm.paint = JSON.parse(detail);
         }
 
+        // Get the modal
+        var modal = document.getElementById("modalinfo");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var leave = document.getElementsByClassName("check-out-btn")[0];
+
+        leave.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+                modal.style.display = "block";
+            }
+            // When the user clicks anywhere outside of the modal, close it
+            // window.onclick = function(event) {
+            //     if (event.target == modal) {
+            //         modal.style.display = "none";
+            //     }
+            // }
+
         // vm.addtocart = function() {
         //     var modalInput = document.getElementById("item");
         //     vm.tasks.push(modalInput.value);
