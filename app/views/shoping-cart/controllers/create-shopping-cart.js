a6aa6053b3978b7f210b59957b95b6b29a86d745
unknown <richard.okoye@netopng.com>
2020-06-06 23:28:20 +0100

    working on total price

--- app/views/shoping-cart/controllers/create-shopping-cart.js
+++ app/views/shoping-cart/controllers/create-shopping-cart.js
@@ -5,6 +5,7 @@
         var vm = this;
         // vm.qty = 1;
         vm.cart = [];
+        vm.total = 0;
 
         var detail = localStorage.getItem("itemInCart");
         if (detail !== undefined || detail !== null) {
@@ -35,7 +36,35 @@
         //     }
         // }
 
-        vm.addtocart = function () { }
+        vm.addtocart = function () { };
+
+
+        $scope.remove = function (paint) {
+            var index = $scope.vm.paints.indexOf(paint);
+            $scope.vm.paints.splice(index, 1);
+            localStorage.setItem('itemInCart', JSON.stringify($scope.vm.paints));
+        }
+
+        // Calculate the price of the total items in cart.
+        $scope.getTotal = function () {
+            var total = 0;
+            for (var i = 0; i < vm.cart.itemInCart.length; i++) {
+                var paint = vm.cart.itemInCart[i];
+                total += (paint.price * paint.qty);
+            }
+            return total;
+        }
+
+        $scope.total = function () {
+            var total = 0;
+            angular.forEach($scope.items, function (item) {
+                total += item.price * item.qty;
+            })
+
+            return total;
+        }
+
+
 
     }])
 })();
